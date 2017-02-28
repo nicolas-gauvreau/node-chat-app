b447acd96eb7f1ed83dfa4e2391a80dbc423e1d0
Nicolas Gauvreau <GauvreNi@int.videotron.com>
2017-02-27 20:14:17 -0500

    Add Mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -7,19 +7,28 @@ socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
 socket.on('newMessage', function(newMessage) {
+  var template = jQuery('#message-template').html();
   var formattedTime = moment(newMessage.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${newMessage.from} ${formattedTime}: ${newMessage.text}`);
-  jQuery('#messages').append(li);
+  var html = Mustache.render(template, {
+    text: newMessage.text,
+    from: newMessage.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+
 });
 socket.on('newLocationMessage', function(newMessage) {
+  var template = jQuery('#location-message-template').html();
   var formattedTime = moment(newMessage.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
-  li.text(`${newMessage.from} ${formattedTime}:`);
-  a.attr('href', newMessage.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var html = Mustache.render(template, {
+    from: newMessage.from,
+    url: newMessage.url,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+
 });
 
 jQuery('#message-form').on('submit', function(e) {
